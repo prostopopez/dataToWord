20c2f174f3b915ecd01a1f9c29e960577a8f5e07
prostopopez <gtaas@mail.ru>
2020-11-11 09:38:31 +0300

    change

--- static-file.js
+++ static-file.js
@@ -3,6 +3,7 @@ let express = require('express');
 let tempfile = require('tempfile');
 let officegen = require('officegen');
 let docx = officegen('docx');
+let path = require('path');
 
 // Express-приложение
 let app = express();
@@ -10,45 +11,55 @@ let app = express();
 app.use("/static", express.static('./static/'));
 // Маршрут для главной страницы
 app.get('/', function (req, res) {
-    res.sendFile('index.html', { root: __dirname });
+    res.sendFile('index.html', {root: __dirname});
 });
 
 // Парсер
 const parseData = (req, res, next) => {
     if (req.method === 'POST') {
-        const formData = {}
+        const formData = {};
         req.on('data', data => {
 
             // Расшифровка данных
             const parsedData =
-                decodeURIComponent(data).split('&')
+                decodeURIComponent(data).split('&');
 
             for (let data of parsedData) {
 
                 decodedData = decodeURIComponent(
-                    data.replace(/\+/g, '%20'))
+                    data.replace(/\+/g, '%20'));
 
                 const [key, value] =
-                    decodedData.split('=')
+                    decodedData.split('=');
 
                 // Данные => объект
-                formData[key] = value
+                formData[key] = value;
             }
 
             // Данные формы => объект запроса
-            req.body = formData
-            next()
-        })
+            req.body = formData;
+            next();
+        });
     } else {
-        next()
+        next();
     }
-}
+};
 
 // Вывод информации
 app.post('/finalData', parseData, (req, res) => {
     // Получение данных формы из объекта запроса
     const data = req.body;
-    const { cardNumber, paramedicName } = data;
+    const {
+        mainDate, cardNumber, paramedicOnCall,
+        callReceiveTime, callTransferTime, teamCallTime, arrivalTime, transportStartTime, transportEndTime, callEndTime, isAirTransport,
+        startCity, startHealthCareFacility, startPhoneNumber, startNameAndPost, directionalDiagnosis,
+        evacuationAgreementCity, evacuationAgreementFacility, evacuationAgreementPhoneNumber, evacuationAgreementNameAndPost,
+        call, callCause,
+        name, surname, patronymic, gender, birthDate, regPlace, jobPlace, insuranceCompany, insurancePolicy, passport,
+        mainDiagnosis, complications, accompanying,
+        driveResult, hospitalizationPlace,
+        brigadeDoctor, additionalDoctor, paramedicInBrigade, nurseInBrigade, driverInBrigade, driveDistance, complains, anamnesis
+    } = data;
 
     // Создание Word файла
     let tempFilePath = tempfile('.docx');
@@ -57,8 +68,10 @@ app.post('/finalData', parseData, (req, res) => {
     docx.setDescription('test description');
 
     // Запись данных
-    let pObj = docx.createP({ align: 'center' });
-    pObj.addText(`${cardNumber}, ${paramedicName}`, { bold: true, underline: true });
+    let pObj = docx.createP({align: 'center'});
+    pObj.addText('Нижнетагильский филиал', {font_size: 16, bold: true, font_face: 'Times New Roman'});
+    pObj.addImage(path.resolve(__dirname, 'static/img/printingImage.png'), {align: 'right', x:0});
+    // pObj.addText(`${mainDate}, ${cardNumber}`, {bold: true, underline: true});
 
     // Завершение
     docx.on('finalize', function (written) {
@@ -74,7 +87,7 @@ app.post('/finalData', parseData, (req, res) => {
         'Content-disposition': 'attachment; filename=testdoc.docx'
     });
     docx.generate(res);
-})
+});
 
 // Сервер на порту 8080
 app.listen(8080);
