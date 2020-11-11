07fe438d2332134ba2ecebd18c4b99626e330306
glebkupriyanko <glebkupriyanko>
2020-11-11 16:30:54 +0300

    dates

--- static-file.js
+++ static-file.js
@@ -1,9 +1,16 @@
 // Express, tempfile, officegen
 let express = require('express');
+let path = require('path');
 let tempfile = require('tempfile');
 let officegen = require('officegen');
-let docx = officegen('docx');
-let path = require('path');
+let docx = officegen({
+    type: 'docx',
+    subject: 'Report Name',
+    keywords: 'Reports',
+    orientation: 'portrait',
+    description: 'A Report Description',
+    pageMargins: { top: 100, left: 400, bottom: 100, right: 400 }
+});
 
 // Express-приложение
 let app = express();
@@ -11,7 +18,7 @@ let app = express();
 app.use("/static", express.static('./static/'));
 // Маршрут для главной страницы
 app.get('/', function (req, res) {
-    res.sendFile('index.html', {root: __dirname});
+    res.sendFile('index.html', { root: __dirname });
 });
 
 // Парсер
@@ -25,7 +32,6 @@ const parseData = (req, res, next) => {
                 decodeURIComponent(data).split('&');
 
             for (let data of parsedData) {
-
                 decodedData = decodeURIComponent(
                     data.replace(/\+/g, '%20'));
 
@@ -49,7 +55,7 @@ const parseData = (req, res, next) => {
 app.post('/finalData', parseData, (req, res) => {
     // Получение данных формы из объекта запроса
     const data = req.body;
-    const {
+    let {
         mainDate, cardNumber, paramedicOnCall,
         callReceiveTime, callTransferTime, teamCallTime, arrivalTime, transportStartTime, transportEndTime, callEndTime, isAirTransport,
         startCity, startHealthCareFacility, startPhoneNumber, startNameAndPost, directionalDiagnosis,
@@ -61,6 +67,37 @@ app.post('/finalData', parseData, (req, res) => {
         brigadeDoctor, additionalDoctor, paramedicInBrigade, nurseInBrigade, driverInBrigade, driveDistance, complains, anamnesis
     } = data;
 
+    // Проверка на заполнение
+    function testIfUndefined(element) {
+        if (element == 'undefined') {
+            return '';
+        }
+        return element;
+    }
+
+    for (let dataElement in data) {
+        if (data.hasOwnProperty(dataElement)) {
+            testIfUndefined(dataElement);
+        }
+    }
+
+    // Форматирование дат
+    [callReceiveTime, callTransferTime, teamCallTime, arrivalTime, transportStartTime, transportEndTime, callEndTime] = [callReceiveTime, callTransferTime, teamCallTime, arrivalTime, transportStartTime, transportEndTime, callEndTime].map(date => {
+        if (date == 'undefined') {
+            return 'aa';
+        } else {
+            return date.split(/\r?\n/)[0].split(/[-T]+/)[2] + '.' +
+                date.split(/\r?\n/)[0].split(/[-T]+/)[1] + '.' +
+                date.split(/\r?\n/)[0].split(/[-T]+/)[0] + ', ' +
+                date.split(/\r?\n/)[0].split(/[-T]+/)[3];
+        }
+    });
+
+    birthDate = birthDate.split(/\r?\n/)[0].split(/[-]+/)[2] + '.' +
+        birthDate.split(/\r?\n/)[0].split(/[-]+/)[1] + '.' +
+        birthDate.split(/\r?\n/)[0].split(/[-]+/)[0];
+
+
     // Создание Word файла
     let tempFilePath = tempfile('.docx');
     docx.setDocSubject('testDoc Subject');
@@ -68,10 +105,42 @@ app.post('/finalData', parseData, (req, res) => {
     docx.setDescription('test description');
 
     // Запись данных
-    let pObj = docx.createP({align: 'center'});
-    pObj.addText('Нижнетагильский филиал', {font_size: 16, bold: true, font_face: 'Times New Roman'});
-    pObj.addImage(path.resolve(__dirname, 'static/img/printingImage.png'), {align: 'right', x:0});
-    // pObj.addText(`${mainDate}, ${cardNumber}`, {bold: true, underline: true});
+    let pObj = docx.createP({ align: 'center' });
+    pObj.addText('Нижнетагильский филиал', { font_size: 16, bold: true, font_face: 'Times New Roman' });
+    pObj.addImage(path.resolve(__dirname, 'static/img/printingImage.png'));
+    pObj.addLineBreak();
+    let table = [
+        [{
+            val: "1. Время (часы, минуты)",
+            opts: {
+                gridSpan: 8,
+                align: 'center',
+                b: true,
+                sz: 20,
+                fontFamily: "Times New Roman"
+            }
+        }],
+        [`Прием вызова`, `Передача вызова бригаде`, `Выезд бригады`, `Прибытие на место вызова`,
+            `Начало транспортировки / убытие`, `Окончание транспортировки`, `Окончание вызова`, `Авиатранспорт`],
+        [`${callReceiveTime}`, `${callTransferTime}`, `${teamCallTime}`, `${arrivalTime}`, `${transportStartTime}`,
+            `${transportEndTime}`, `${callEndTime}`, `${isAirTransport}`],
+
+    ]
+    let tableStyle = {
+        borders: true,
+        tableColWidth: 4261,
+        tableSize: 24,
+        sz: 20,
+        tableColor: "ada",
+        tableAlign: "left",
+        tableFontFamily: "Times New Roman"
+    }
+    let tableInfo = [{
+        type: "table",
+        val: table,
+        opt: tableStyle
+    }]
+    docx.createByJson(tableInfo);
 
     // Завершение
     docx.on('finalize', function (written) {
